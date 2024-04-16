de679fa6d99a61676f89373f1c35eaff2d8b1fb1
Hanhan <764932129@qq.com>
2024-04-17 00:44:57 +0800

    feat: implement method of "hasPermission"

--- src/main/java/me/xiaoying/livegetauthorize/server/entity/Console.java
+++ src/main/java/me/xiaoying/livegetauthorize/server/entity/Console.java
@@ -6,4 +6,8 @@ import me.xiaoying.livegetauthorize.core.command.CommandSender;
  * Entity CommandSender Console
  */
 public class Console implements CommandSender {
+    @Override
+    public boolean hasPermission(String s) {
+        return true;
+    }
 }
