a401569118d608ed67b42dad4144f71d1225bf9e
Hanhan <764932129@qq.com>
2024-04-16 14:18:23 +0800

    feat: make new CommandSender which named "Console"

---
+++ src/main/java/me/xiaoying/livegetauthorize/server/entity/Console.java
@@ -0,0 +1,9 @@
+package me.xiaoying.livegetauthorize.server.entity;
+
+import me.xiaoying.livegetauthorize.core.command.CommandSender;
+
+/**
+ * Entity CommandSender Console
+ */
+public class Console implements CommandSender {
+}
