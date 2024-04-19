ae7b47063ea1718a45a1edb9b047a5a55236288c
Hanhan <764932129@qq.com>
2024-04-19 20:52:10 +0800

    feat: make method of "getSqlFactory" for Application

--- src/main/java/me/xiaoying/livegetauthorize/server/Application.java
+++ src/main/java/me/xiaoying/livegetauthorize/server/Application.java
@@ -3,12 +3,16 @@ package me.xiaoying.livegetauthorize.server;
 import me.xiaoying.livegetauthorize.core.LACore;
 import me.xiaoying.livegetauthorize.core.server.Server;
 import me.xiaoying.livegetauthorize.server.command.StopCommand;
+import me.xiaoying.livegetauthorize.server.constant.FileConfigConstant;
 import me.xiaoying.livegetauthorize.server.file.FileService;
 import me.xiaoying.livegetauthorize.server.file.files.FileConfig;
 import me.xiaoying.livegetauthorize.server.listener.LoggerListener;
 import me.xiaoying.livegetauthorize.server.terminal.Terminal;
 import me.xiaoying.logger.LoggerFactory;
 import me.xiaoying.logger.event.EventHandle;
+import me.xiaoying.sql.MysqlFactory;
+import me.xiaoying.sql.SqlFactory;
+import me.xiaoying.sql.SqliteFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -68,5 +72,15 @@ public class Application {
 
     public static FileService getFileService() {
         return fileService;
+    }
+
+    public static SqlFactory getSqlFactory() {
+        switch (FileConfigConstant.SETTING_DATA_TYPE.toUpperCase()) {
+            case "MYSQL":
+                return new MysqlFactory(FileConfigConstant.SETTING_DATA_MYSQL_HOSTNAME, FileConfigConstant.SETTING_DATA_MYSQL_PORT, FileConfigConstant.SETTING_DATA_MYSQL_DATABASE, FileConfigConstant.SETTING_DATA_MYSQL_USERNAME, FileConfigConstant.SETTING_DATA_MYSQL_PASSWORD);
+            case "SQLITE":
+                return new SqliteFactory(FileConfigConstant.SETTING_DATA_SQLITE_PATH, FileConfigConstant.SETTING_DATA_SQLITE_FILE);
+        }
+        return null;
     }
 }
