ef5f34252079c25f4188a8c76d8bbca2e9f6cd5f
Hanhan <764932129@qq.com>
2024-04-25 13:30:10 +0800

    feat: Constant

---
+++ src/main/java/me/xiaoying/livegetauthorize/server/constant/ConstantCommon.java
@@ -0,0 +1,8 @@
+package me.xiaoying.livegetauthorize.server.constant;
+
+/**
+ * Constant Common
+ */
+public class ConstantCommon {
+    public static String TABLE_USER_INFO = "user_info";
+}
---
+++ src/main/java/me/xiaoying/livegetauthorize/server/constant/FileMessageConstant.java
@@ -0,0 +1,27 @@
+package me.xiaoying.livegetauthorize.server.constant;
+
+/**
+ * Constant FileMessage
+ */
+public class FileMessageConstant {
+    public static String MESSAGE_ACCOUNT_LOGIN ,
+            MESSAGE_ACCOUNT_NOT_FOUND,
+            MESSAGE_ACCOUNT_PASSWORD_INVALID;
+
+    static {
+        MESSAGE_ACCOUNT_LOGIN = "{\n" +
+                "  \"code\": 200,\n" +
+                "  \"account\": \"%account%\",\n" +
+                "  \"token\": \"%token%\",\n" +
+                "  \"message\": \"Login success.\"\n" +
+                "}";
+        MESSAGE_ACCOUNT_NOT_FOUND = "{\n" +
+                "  \"code\": 101,\n" +
+                "  \"message\": \"can't find user.\"\n" +
+                "}";
+        MESSAGE_ACCOUNT_PASSWORD_INVALID = "{\n" +
+                "  \"code\": 301,\n" +
+                "  \"message\": \"Password invalid.\"\n" +
+                "}";
+    }
+}
