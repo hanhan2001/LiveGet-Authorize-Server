38768d94022751520f642b2eb6c236a1136b8608
Hanhan <764932129@qq.com>
2024-04-25 15:01:23 +0800

    feat: Long util

---
+++ src/main/java/me/xiaoying/livegetauthorize/server/utils/LongUtil.java
@@ -0,0 +1,21 @@
+package me.xiaoying.livegetauthorize.server.utils;
+
+/**
+ * Util Long
+ */
+public class LongUtil {
+    /**
+     * 是否为 Long
+     *
+     * @param string String
+     * @return Boolean
+     */
+    public static boolean isLong(String string) {
+        try {
+            Long.parseLong(string);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
