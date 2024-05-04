8929703e6e2613f785c4254d9a75a678d917323b
Hanhan <764932129@qq.com>
2024-05-04 10:59:19 +0800

    fix: can't use "url/index" to get web index

--- src/main/java/me/xiaoying/livegetauthorize/server/controller/IndexController.java
+++ src/main/java/me/xiaoying/livegetauthorize/server/controller/IndexController.java
@@ -1,14 +1,14 @@
 package me.xiaoying.livegetauthorize.server.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Page index
  */
-@RestController
+@Controller
 public class IndexController {
-    @GetMapping("/index")
+    @RequestMapping("/index")
     public String index() {
         return "index";
     }
