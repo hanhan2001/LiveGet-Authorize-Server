113ebda5ced18af69c848e53aac3e90692476818
Hanhan <764932129@qq.com>
2024-05-06 14:29:32 +0800

    feat: make new method of "qq" for VariableFactory

--- src/main/java/me/xiaoying/livegetauthorize/server/factory/VariableFactory.java
+++ src/main/java/me/xiaoying/livegetauthorize/server/factory/VariableFactory.java
@@ -15,6 +15,16 @@ public class VariableFactory {
         return this;
     }
 
+    public VariableFactory qq(String qq) {
+        this.string = this.string.replace("%qq%", qq);
+        return this;
+    }
+
+    public VariableFactory qq(long qq) {
+        this.string = this.string.replace("%qq%", String.valueOf(qq));
+        return this;
+    }
+
     public VariableFactory token(String token) {
         this.string = this.string.replace("%token%", token);
         return this;
