c0b33edcd3a27c988880d08357b6ba13d2635699
Hanhan <764932129@qq.com>
2024-05-15 17:50:29 +0800

    fix: can't create new user

--- src/main/java/me/xiaoying/livegetauthorize/server/user/UserManager.java
+++ src/main/java/me/xiaoying/livegetauthorize/server/user/UserManager.java
@@ -97,7 +97,7 @@ public class UserManager {
         Insert insert = new Insert(ConstantCommon.TABLE_USER_INFO);
         Date date = new Date();
         String stringDate = DateUtil.dateToString(date, FileConfigConstant.SETTING_DATEFORMAT);
-        insert.insert(String.valueOf(qq), email, "", password, uuid, "", stringDate, stringDate);
+        insert.insert(String.valueOf(qq), email, "", password, uuid, String.valueOf(qq), "", stringDate, stringDate);
         sqlFactory.sentence(insert).run();
         User user = new ServerUser(qq, email, uuid, password, "", date, date);
         this.knownQQUsers.put(qq, user);
