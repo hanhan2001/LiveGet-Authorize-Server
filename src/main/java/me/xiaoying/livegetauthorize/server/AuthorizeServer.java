2bb54f7147c7cdb2c8de9f3a6952e2c9ad83c08a
Hanhan <764932129@qq.com>
2024-05-29 22:09:44 +0800

    style: remove useless code

--- src/main/java/me/xiaoying/livegetauthorize/server/AuthorizeServer.java
+++ src/main/java/me/xiaoying/livegetauthorize/server/AuthorizeServer.java
@@ -14,10 +14,7 @@ import me.xiaoying.livegetauthorize.core.server.Server;
 import me.xiaoying.livegetauthorize.server.classification.SimpleClassificationManager;
 import me.xiaoying.livegetauthorize.server.scheduler.ServerScheduler;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 
 /**
  * Server Authorize
