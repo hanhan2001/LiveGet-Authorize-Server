475789510712e1e5b64577908170fae0081ffab4
Hanhan <764932129@qq.com>
2024-05-30 17:53:32 +0800

    feat: remove useless code

--- src/main/java/me/xiaoying/livegetauthorize/server/option/SimpleOptionManager.java
+++ src/main/java/me/xiaoying/livegetauthorize/server/option/SimpleOptionManager.java
@@ -37,7 +37,6 @@ public class SimpleOptionManager implements OptionManager {
 
     private void defaultOption() {
         SqlFactory sqlFactory = Application.getSqlFactory();
-        System.out.println(sqlFactory.getTotalRecord(ConstantCommon.OPTION_LIST));
         if (sqlFactory.getTotalRecord(ConstantCommon.OPTION_LIST) > 0)
             return;
 
